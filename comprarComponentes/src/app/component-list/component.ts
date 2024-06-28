976de7619f11615dd0f388e48fe2bd53a4f2b717
KhadaGeno <143861134+KhadaGeno@users.noreply.github.com>
2024-06-28 18:48:28 -0300

    Add files via upload

---
+++ comprarComponentes/src/app/component-list/component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+export interface Component_type {
+    name: String;
+    type: String;
+    price: number;
+    stock: number;
+    clearance: boolean;
+    quantity: number;
+}
